0b8345cad47f3a0c38c78df124a0ac36c860d228
Carlos Saito <carlos@saito.style>
2020-01-04 21:56:21 +0100

    Rename components directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Timeline from "./components/timeline"
-import Logo from "./components/logo"
+import Timeline from "../components/timeline"
+import Logo from "../components/logo"
 
 export default () => (
   <div>
