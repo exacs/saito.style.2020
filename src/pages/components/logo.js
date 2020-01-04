ffd1ab05aa3578b41eae9681bfe557ce9ff18a28
Carlos Saito <carlos@saito.style>
2020-01-04 20:29:04 +0100

    Implement the "X" logo

--- src/pages/components/logo.js
+++ src/pages/components/logo.js
@@ -1,3 +1,6 @@
 import React from "react"
 
-export default () => <div>Logo</div>
+export default () => <svg width={192} height={256} viewBox="0 0 192 256">
+  <path fill="blue" d="M 0 0    l 160 256  l 32 0 l -160 -256" />
+  <path fill="black" d="M 0 256  l 160 -256 l 32 0 l -160 256" />
+</svg>
