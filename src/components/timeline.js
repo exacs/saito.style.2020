d7fa9b106783f8a839fdaf1ad2374210a3695ef2
Carlos Saito <carlos@saito.style>
2020-01-05 12:14:41 +0100

    Fix styles

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -3,27 +3,18 @@ import React from "react"
 export default () => (
   <div>
     <section>
-      <div>2018 - present</div>
-      <h2>Developer consultant at KTH's e-learning team</h2>
-      <div>
-        <div>
+      <div className='font-bold'>Current job</div>
+      <h2 className='text-2xl font-bold'>Developer consultant at KTH's e-learning team</h2>
+      <div className='flex -mx-2'>
+        <div className='w-48 text-sm mx-2'>
           <div>Company</div>
           <div>Find-out Technologies AB</div>
         </div>
-        <div>
+        <div className='w-48 text-sm mx-2'>
           <div>Customer</div>
           <div>KTH Royal Institute of Technology</div>
         </div>
       </div>
-      <main className='font-serif'>
-        Developer at e-learning team of the KTH. Mainly write Node.JS
-        open-source apps (github.com/kth) to sync data between KTH’s internal
-        systems and Canvas –an externally developed Learning Management System–;
-        create external tools integrated in Canvas like transfer of grades from
-        Canvas to Ladok (Swedish central system); also other tasks like help to
-        migrate and shutdown old databases and help with the digitalisation
-        goals of the institution.
-      </main>
     </section>
   </div>
 )
