d4ebe6e9bab1e44be524cd44908c57296663357d
Carlos Saito <carlos@saito.style>
2020-01-05 11:34:21 +0100

    Minor fix

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => (
   <svg width={192} height={256} viewBox="0 0 192 256">
-    <path fill="blue" d="M 0 0    l 160 256  l 32 0 l -160 -256" />
+    <path fill="white" d="M 0 0    l 160 256  l 32 0 l -160 -256" />
     <path fill="black" d="M 0 256  l 160 -256 l 32 0 l -160 256" />
   </svg>
 )
