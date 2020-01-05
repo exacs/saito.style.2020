dab5fb5f549478452c0ab69b9f2011fc14938da6
Carlos Saito <carlos@saito.style>
2020-01-05 11:30:57 +0100

    Add indigo to TailwindCSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
 import React from "react"
 import "./layout.css"
 
-export default ({ children }) => <div className='font-sans'>{children}</div>
+export default ({ children }) => <div className='font-sans bg-indigo-900 text-white'>{children}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   theme: {
+    colors: {
+      white: defaultTheme.colors.white,
+      black: defaultTheme.colors.black,
+      transparent: defaultTheme.colors.transparent,
+      indigo: defaultTheme.colors.indigo
+    },
     extend: {
       fontFamily: {
         serif: [
