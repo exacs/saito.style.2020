6edc8135e82a5e870c4201804905f9e3fa2d2051
Carlos Saito <carlos@saito.style>
2020-01-05 11:50:10 +0100

    Add page title via Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,12 @@
 import React from "react"
+import Helmet from "react-helmet"
 import "./layout.css"
 
-export default ({ children }) => <div className='p-4 font-sans bg-indigo-900 text-white'>{children}</div>
+export default ({ children }) => (
+  <div>
+    <Helmet>
+      <title>saito.style</title>
+    </Helmet>
+    <div className='p-4 font-sans bg-indigo-900 text-white'>{children}</div>
+  </div>
+)
