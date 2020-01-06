971114f7ccc638a0c81310dc37355d64fff19861
Carlos Saito <carlos@saito.style>
2020-01-06 12:56:50 +0100

    Add full-time/sporadic icons

---
+++ src/components/circle-icon.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default ({className}) => (
+  <svg viewBox="0 0 24 24" width="24" height="24" fill="none" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <circle cx="12" cy="12" r="10"></circle>
+  </svg>
+)
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Circle from "./circle-icon"
 
 export default () => (
   <div>
@@ -15,16 +16,24 @@ export default () => (
           <div className='text-indigo-100'>KTH Royal Institute of Technology</div>
         </div>
       </div>
+      <div className='font-bold text-indigo-200'>
+        <Circle className='inline-block stroke-current align-middle w-4 h-4' />
+        <div className='inline-block px-2 align-middle'>Full-time</div>
+      </div>
     </section>
 
     <section className='my-16'>
-      <div className='font-bold text-indigo-100'>Current job</div>
+      <div className='font-bold text-indigo-100'>Current project</div>
       <h2 className='text-xl font-bold my-2'>Freelance full-stack developer for COAM's magazine</h2>
       <div className='my-4'>
         <div className='my-4 text-sm'>
           <div className='text-indigo-300'>Customer</div>
           <div className='text-indigo-100'>COAM (Official Association of Architects of Madrid)</div>
         </div>
+      </div>
+      <div className='font-bold text-indigo-200'>
+        <Circle className='inline-block stroke-current align-middle w-4 h-4' />
+        <div className='inline-block px-2 align-middle'>Sporadic</div>
       </div>
     </section>
 
@@ -36,6 +45,10 @@ export default () => (
           <div className='text-indigo-300'>Institution</div>
           <div className='text-indigo-100'>UPM (Technical University of Madrid)</div>
         </div>
+      </div>
+      <div className='font-bold text-indigo-200'>
+        <Circle className='inline-block stroke-current align-middle w-4 h-4' />
+        <div className='inline-block px-2 align-middle'>Full-time</div>
       </div>
     </section>
   </div>
