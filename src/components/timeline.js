b6ac9e10ccbf707e23a3ea508028571f5549a6c1
Carlos Saito <carlos@saito.style>
2020-01-06 11:02:57 +0100

    Add a second job

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export default () => (
   <div>
-    <section>
-      <div className='font-bold'>Current job</div>
-      <h2 className='text-2xl font-bold my-2'>Developer consultant at KTH's e-learning team</h2>
+    <section className='my-16'>
+      <div className='font-bold text-indigo-100'>Current job</div>
+      <h2 className='text-xl font-bold my-2'>Developer consultant at KTH's e-learning team</h2>
       <div className='my-4'>
         <div className='my-4 text-sm'>
           <div className='text-indigo-300'>Company</div>
@@ -13,6 +13,17 @@ export default () => (
         <div className='my-4 text-sm'>
           <div className='text-indigo-300'>Customer</div>
           <div className='text-indigo-100'>KTH Royal Institute of Technology</div>
+        </div>
+      </div>
+    </section>
+
+    <section className='my-16'>
+      <div className='font-bold text-indigo-100'>Current job</div>
+      <h2 className='text-xl font-bold my-2'>Freelance full-stack developer for COAM's magazine</h2>
+      <div className='my-4'>
+        <div className='my-4 text-sm'>
+          <div className='text-indigo-300'>Customer</div>
+          <div className='text-indigo-100'>COAM (Official Association of Architects of Madrid)</div>
         </div>
       </div>
     </section>
