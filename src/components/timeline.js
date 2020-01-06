a9726c39110b3b58a7f6e42be04f73926dfbdd78
Carlos Saito <carlos@saito.style>
2020-01-06 10:49:16 +0100

    Fix layout

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,15 +4,15 @@ export default () => (
   <div>
     <section>
       <div className='font-bold'>Current job</div>
-      <h2 className='text-2xl font-bold'>Developer consultant at KTH's e-learning team</h2>
-      <div className='flex -mx-2'>
-        <div className='w-48 text-sm mx-2'>
-          <div>Company</div>
-          <div>Find-out Technologies AB</div>
+      <h2 className='text-2xl font-bold my-2'>Developer consultant at KTH's e-learning team</h2>
+      <div className='my-4'>
+        <div className='my-4 text-sm'>
+          <div className='text-indigo-300'>Company</div>
+          <div className='text-indigo-100'>Find-out Technologies AB</div>
         </div>
-        <div className='w-48 text-sm mx-2'>
-          <div>Customer</div>
-          <div>KTH Royal Institute of Technology</div>
+        <div className='my-4 text-sm'>
+          <div className='text-indigo-300'>Customer</div>
+          <div className='text-indigo-100'>KTH Royal Institute of Technology</div>
         </div>
       </div>
     </section>
