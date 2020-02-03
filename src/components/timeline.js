b8020c0bd88f59e81466e3dc8191d6afd8d438e5
Carlos Saito <carlos@saito.style>
2020-02-03 09:32:37 +0100

    More things into the timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -3,8 +3,9 @@ import Circle from "./circle-icon"
 
 export default () => (
   <div>
-    <section className="my-16">
-      <div className="font-bold text-indigo-100">Current job</div>
+    <h1 className="text-5xl font-bold text-indigo-100 my-4">2018 - 2020</h1>
+    <section className="mb-16">
+      <div className="font-bold text-indigo-100">2018/jun - present</div>
       <h2 className="text-xl font-bold my-2">
         Developer consultant at KTH's e-learning team
       </h2>
@@ -34,8 +35,8 @@ export default () => (
       </div>
     </section>
 
-    <section className="my-16">
-      <div className="font-bold text-indigo-100">Current project</div>
+    <section className="mb-16">
+      <div className="font-bold text-indigo-100">2017/dic - present</div>
       <h2 className="text-xl font-bold my-2">
         Freelance full-stack developer for COAM's magazine
       </h2>
@@ -58,7 +59,8 @@ export default () => (
       </div>
     </section>
 
-    <section className="my-16">
+    <h1 className="text-5xl font-bold text-indigo-100 my-4">2013 - 2018</h1>
+    <section className="mb-16">
       <div className="font-bold text-indigo-100">2013/sep - 2018/jun</div>
       <h2 className="text-xl font-bold my-2">
         Computer Enginner Undergraduate
@@ -70,10 +72,84 @@ export default () => (
             UPM (Technical University of Madrid)
           </div>
         </div>
+
+        <div className="my-4 text-sm">
+          <div className="text-indigo-300">Exchange studies (ERASMUS)</div>
+          <div className="text-indigo-100">
+            Uppsala university
+          </div>
+        </div>
       </div>
       <div className="font-bold text-indigo-200">
         <Circle className="inline-block stroke-current align-middle w-4 h-4" />
         <div className="inline-block px-2 align-middle">Full-time</div>
+      </div>
+    </section>
+
+    <section className="mb-16">
+      <div className="font-bold text-indigo-100">2017/feb - 2017/oct</div>
+      <h2 className="text-xl font-bold my-2">
+        Developer trainee
+      </h2>
+      <div className="my-4">
+        <div className="my-4 text-sm">
+          <div className="text-indigo-300">Organisation</div>
+          <div className="text-indigo-100">
+            Common Action Forum
+          </div>
+        </div>
+      </div>
+      <div className="font-bold text-indigo-200">
+        <Circle className="inline-block stroke-current align-middle w-4 h-4" />
+        <div className="inline-block px-2 align-middle">Part-time</div>
+      </div>
+
+      <div className="font-serif text-indigo-100 my-4">
+        Developer of the “Ontag” project, an open-source tool for journalists aimed to give answers to relevant questions through annotations on news articles. Part of a multidisciplinary and international team formed by journalists and sociologists of Common Action Forum and computer engineers of the Ontology Engineering Group.
+      </div>
+    </section>
+
+    <section className="mb-16">
+      <div className="font-bold text-indigo-100">2016/mar - 2016/jun</div>
+      <h2 className="text-xl font-bold my-2">
+        Part-time front end developer
+      </h2>
+      <div className="my-4">
+        <div className="my-4 text-sm">
+          <div className="text-indigo-300">Company</div>
+          <div className="text-indigo-100">
+            Coowry LTD
+          </div>
+        </div>
+      </div>
+      <div className="font-serif text-indigo-100 my-4">
+        Web developer at Coowry, a web application used for transferring money (up to USD 10) from mobile to mobile. Enhanced the look and feel of the page introducing the Bootstrap framework. Also improved the tools "ag", used by merchants; and "master", an internal tool for analytics.
+      </div>
+      <div className="font-bold text-indigo-200">
+        <Circle className="inline-block stroke-current align-middle w-4 h-4" />
+        <div className="inline-block px-2 align-middle">Part-time</div>
+      </div>
+    </section>
+
+    <section className="mb-16">
+      <div className="font-bold text-indigo-100">2014 - 2016</div>
+      <h2 className="text-xl font-bold my-2">
+        Web Development intern
+      </h2>
+      <div className="my-4">
+        <div className="my-4 text-sm">
+          <div className="text-indigo-300">Organisation</div>
+          <div className="text-indigo-100">
+            Architectural Design Department, UPM
+          </div>
+        </div>
+      </div>
+      <div className="font-serif text-indigo-100 my-4">
+      I have continued the development of the Wordpress theme of the site. In addition, I have developed some simple web apps very quickly. For example a PhD Lectures Directory was made with AngularJS since we needed a simple directory.
+      </div>
+      <div className="font-bold text-indigo-200">
+        <Circle className="inline-block stroke-current align-middle w-4 h-4" />
+        <div className="inline-block px-2 align-middle">Part-time</div>
       </div>
     </section>
   </div>
